7eb1e607ca03dea3d8e2ceb81788bb4366e77529
Alma Marie <daena76@gmail.com>
2021-12-27 21:14:18 +0000

    My first Commit

---
+++ file.sh
@@ -0,0 +1,11 @@
+	Types of Version Control System
+	- Local Version Control
+	Pros: Write and code changes are kept in a local computer
+	Cons: Multiple people parallelly working on same project
+	
+	- Centralized Version Control
+	Pros: Write and Commit all codes in one server
+	Cons: When servers go down, no way to access the code
+	
+	- Distributed Version Control (we will use this!)
+Pros: Write and Commit all codes on local systems and remote server
